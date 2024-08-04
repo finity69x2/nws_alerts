f9c6807cd31571e15c72ecb84f64305c929a98d3
finity69x2 <32221243+finity69x2@users.noreply.github.com>
2024-08-04 14:47:59 -0400

    Update __init__.py

--- custom_components/nws_alerts/__init__.py
+++ custom_components/nws_alerts/__init__.py
@@ -192,6 +192,7 @@ async def async_get_state(config, coords) -> dict:
         "message_type": None,
         "event_status": None,
         "event_severity": None,
+        "event_sent": None,
         "event_onset": None,
         "event_expires": None,
         "display_desc": None,
@@ -276,6 +277,7 @@ async def async_get_alerts(zone_id: str = "", gps_loc: str = "") -> dict:
             tmp_dict["Status"] = alert["properties"]["status"]
             tmp_dict["Severity"] = alert["properties"]["severity"]
             tmp_dict["Certainty"] = alert["properties"]["certainty"]
+            tmp_dict["Sent"] = alert["properties"]["sent"]
             tmp_dict["Onset"] = alert["properties"]["onset"]
             tmp_dict["Expires"] = alert["properties"]["expires"]
             tmp_dict["Description"] = alert["properties"]["description"]
